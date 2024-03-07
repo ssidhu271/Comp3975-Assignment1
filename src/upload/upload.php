ce9dea817f7714f5596e7dca7118fd9fe58d871a
ssidhu271 <ssidhu271@my.bcit.ca>
2024-03-06 18:02:43 -0800

    Changing upload method

--- src/upload/upload.php
+++ src/upload/upload.php
@@ -13,27 +13,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['transactionFile'])) 
     $fileType = strtolower(pathinfo($uploadFile, PATHINFO_EXTENSION));
     if ($fileType !== 'csv') {
         $_SESSION['error-message'] = "Error: Only CSV files are allowed.";
-        header("Location: ../index.php");
-        exit;
-    }
-
-    if (!is_uploaded_file($_FILES['transactionFile']['tmp_name'])) {
+    } elseif (!is_uploaded_file($_FILES['transactionFile']['tmp_name'])) {
         $_SESSION['error-message'] = "No file was uploaded.";
     } elseif (!is_writable($uploadDir)) {
         $_SESSION['error-message'] = "Upload directory is not writable.";
     } elseif (move_uploaded_file($_FILES['transactionFile']['tmp_name'], $uploadFile)) {
-        echo "The file " . htmlspecialchars(basename($_FILES['transactionFile']['name'])) . " has been uploaded.";
+        // echo "The file " . htmlspecialchars(basename($_FILES['transactionFile']['name'])) . " has been uploaded.";
         // Call the import function
         $db = connect_database();
         importCSVToSQLite($uploadFile, $db);
         $_SESSION['message'] = "The file has been uploaded and imported.";
-        header("Location: ../index.php");
     } else {
         $_SESSION['error-message'] = "There was an error uploading your file.";
     }
+
+    header("Location: ../index.php");
+    exit;
 } else {
     $_SESSION['error-message'] = "No file uploaded or wrong method used.";
+    header("Location: ../index.php");
+    exit;
 }
-
-header("Location: ../index.php");
-exit;
